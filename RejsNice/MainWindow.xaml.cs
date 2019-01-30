2a693684ed524d4641e20d3f698e5664f3d6c2e8
Anders Dalsgaard Madsen <ande48d0@edu.eal.dk>
2019-01-30 11:47:52 +0100

    Tid done (hour og minute i combobox)

--- RejsNice/MainWindow.xaml.cs
+++ RejsNice/MainWindow.xaml.cs
@@ -23,7 +23,42 @@ namespace RejsNice
         public MainWindow()
         {
             InitializeComponent();
+
+            //Timer i ComboBox
+            HourBox.Items.Add("8");
+            HourBox.Items.Add("9");
+            HourBox.Items.Add("10");
+            HourBox.Items.Add("11");
+            HourBox.Items.Add("12");
+            HourBox.Items.Add("13");
+            HourBox.Items.Add("14");
+            HourBox.Items.Add("15");
+            HourBox.Items.Add("16");
+            HourBox.Items.Add("17");
+            HourBox.Items.Add("18");
+            HourBox.Items.Add("19");
+            HourBox.Items.Add("20");
+            HourBox.Items.Add("21");
+            HourBox.Items.Add("22");
+            HourBox.Items.Add("23");
+            HourBox.Items.Add("00");
+            HourBox.Items.Add("1");
+            HourBox.Items.Add("2");
+            HourBox.Items.Add("3");
+            HourBox.Items.Add("4");
+            HourBox.Items.Add("5");
+            HourBox.Items.Add("6");
+            HourBox.Items.Add("7");
+
+            //Minutter i ComboBox
+            MinuteBox.Items.Add("00");
+            MinuteBox.Items.Add("15");
+            MinuteBox.Items.Add("30");
+            MinuteBox.Items.Add("45");
+
+
         }
-        
+
     }
+    
 }
